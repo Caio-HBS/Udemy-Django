3819652afb6192cf932473d3210a4ccfa1e61472
Caio Bianchi <caiohbs23@gmail.com>
2023-02-27 16:57:36 -0300

    Final version of feedback project

--- feedback/profiles/forms.py
+++ feedback/profiles/forms.py
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 from django import forms
 
 # Sice we're using the CreateView, this class is rendered useless.
 ##class ProfileForm(forms.Form):
     ##user_image = forms.ImageField()
 
-=======
-from django import forms
-
-# Sice we're using the CreateView, this class is rendered useless.
-##class ProfileForm(forms.Form):
-    ##user_image = forms.ImageField()
-
->>>>>>> 7db6920b4a9d41e78a548ec5f18a2925d35d29e3
--- feedback/profiles/urls.py
+++ feedback/profiles/urls.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.urls import path
 
 from . import views
@@ -6,13 +5,4 @@ from . import views
 urlpatterns = [
     path("", views.CreateProfileView.as_view()),
     path("list", views.ProfilesView.as_view())
-=======
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("", views.CreateProfileView.as_view()),
-    path("list", views.ProfilesView.as_view())
->>>>>>> 7db6920b4a9d41e78a548ec5f18a2925d35d29e3
 ]
